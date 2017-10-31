36a65b70b723ad417081ba42cf87e2f13c8d4e62
Mihai <mihai.iftode94@gmail.com>
2017-10-31 18:37:48 +0100

    Add basic movement and detection

---
+++ BirdController.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Morten Nobel-Jørgensen on 19/10/2017.
+//
+
+#include <SDL_events.h>
+#include <iostream>
+#include "BirdController.hpp"
+#include "GameObject.hpp"
+#include "SpriteComponent.hpp"
+#include "PhysicsComponent.hpp"
+#include "SpriteComponent.hpp"
+#include "BirdGame.hpp"
+
+BirdController::BirdController(GameObject* gameObject) : Component(gameObject)
+{
+	auto phys = gameObject->getComponent<PhysicsComponent>();
+	phys->setLinearVelocity({velocityX, 0});
+	// initiate bird physics
+}
+
+void BirdController::init(BirdGame* bird)
+{
+	birdGame = bird;
+}
+
+bool BirdController::onKey(SDL_Event& event)
+{
+	if (event.key.keysym.sym != SDLK_SPACE) return false;
+	auto phys = gameObject->getComponent<PhysicsComponent>();
+	if (event.type == SDL_KEYDOWN)
+	{
+		phys->addImpulse({0, impulseY});
+	}
+	else if (event.type == SDL_KEYUP)
+	{
+	}
+	return false;
+}
+
+void BirdController::onCollisionStart(PhysicsComponent* comp)
+{
+	if (comp->getGameObject()->name == "Wall bottom" || comp->getGameObject()->name == "Wall top")
+	{
+		birdGame->setGameState(GameState::GameOver);
+	}
+}
+
+void BirdController::onCollisionEnd(PhysicsComponent* comp)
+{
+}
---
+++ BirdController.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Component.hpp"
+
+class BirdGame;
+
+class BirdController : public Component
+{
+public:
+	explicit BirdController(GameObject* gameObject);
+
+	void init(BirdGame* bird);
+	bool onKey(SDL_Event& event) override;
+
+	void onCollisionStart(PhysicsComponent* comp) override;
+
+	void onCollisionEnd(PhysicsComponent* comp) override;
+
+private:
+	const float velocityX = 1;
+	const float impulseY = 0.1f;
+	BirdGame * birdGame;
+};
